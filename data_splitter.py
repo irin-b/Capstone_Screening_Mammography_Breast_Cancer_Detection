497503f9891bf6a0081e6d9dda1df3d1b8c34be0
irin-b <108711970+irin-b@users.noreply.github.com>
2023-04-05 00:50:06 +0700

    modules

---
+++ data_splitter.py
@@ -0,0 +1,191 @@
+# importing required modules
+import pandas as pd
+import numpy as np
+import os
+
+from sklearn.model_selection import train_test_split, StratifiedKFold
+
+
+RANDOM_STATE = 42
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+METADATA = pd.read_csv(dir_path + '/' + 'train.csv') # path to metadata csv
+
+class DataSplitter:
+
+    def __init__(self, meta_df=METADATA, label_var='cancer', verbose=False):
+            
+        self.metadata = meta_df
+        self.verbose = verbose
+        self.train, self.test = self.get_train_test_split()
+        self.trainset, self.calibset = self.get_train_calib_split()
+        self.labels = self.get_labels(label_var)
+
+
+    # aggregate cancer result at patient level for stratification
+    def cancer_by_patient(self, meta_df=None, id_subset=None):
+        
+        if meta_df is None:
+            # aggregate cancer result at patient level for stratification
+            patients = self.metadata.groupby('patient_id')['cancer'].sum().reset_index()
+
+        else:
+            patients = meta_df.groupby('patient_id')['cancer'].sum().reset_index()
+        
+        patients['cancer_positive'] = np.where(patients.cancer>=1, 1, 0)
+
+        if id_subset is not None:
+          patients = patients[patients['patient_id'].isin(id_subset)]
+
+        X = patients.drop(columns=['cancer_positive'])
+        y = patients.cancer_positive
+
+        return X, y
+
+
+    # get a dictionary of patient_ids and their corresponding image_ids
+    def get_image_id(self, X_df):
+
+        img_ids = {}
+        for patient_id in X_df.patient_id.unique():
+            img_ids[patient_id] = self.metadata[self.metadata['patient_id']==patient_id].image_id.unique().tolist()
+
+        return img_ids
+
+
+    def get_train_img_id(self):
+
+        img_ids = []
+        for patient_id in self.train.keys():
+            img_ids.extend(self.metadata[self.metadata['patient_id']==patient_id].image_id.unique().tolist())
+
+        return img_ids
+    
+
+    def get_test_img_id(self):
+
+        img_ids = []
+        for patient_id in self.test.keys():
+            img_ids.extend(self.metadata[self.metadata['patient_id']==patient_id].image_id.unique().tolist())
+
+        return img_ids
+    
+
+    # train-test split >> constant throughout the project
+    # hard-coded random state = 42 and proportion at 80:20
+    def get_train_test_split(self):
+
+        X, y = self.cancer_by_patient()
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42, stratify=y)
+
+        if self.verbose==True:
+            print('Total patient_id in training set: ', X_train.patient_id.nunique())
+            print('Total patient_id in test set: ', X_test.patient_id.nunique())
+
+        train = self.get_image_id(X_train)
+        test = self.get_image_id(X_test)
+        
+        if self.verbose==True:
+            print('Total image_id in training set: ', sum([len(val) for key, val in train.items()]))
+            print('Total image_id in test set: ', sum([len(val) for key, val in test.items()]))
+
+        return train, test
+
+    def get_train_calib_split(self, random_state=RANDOM_STATE):
+        
+        train_ids = self.train.keys()
+        X, y = self.cancer_by_patient(id_subset=train_ids)
+        X_train, X_calib, y_train, y_calib = train_test_split(X, y, test_size=0.20, random_state=random_state, stratify=y)
+
+        if self.verbose==True:
+            print('Total patient_id in training set: ', X_train.patient_id.nunique())
+            print('Total patient_id in calibration set: ', X_calib.patient_id.nunique())
+
+        train = self.get_image_id(X_train)
+        calib = self.get_image_id(X_calib)
+
+        if self.verbose==True:
+            print('Total image_id in training set: ', sum([len(val) for key, val in train.items()]))
+            print('Total image_id in calibration set: ', sum([len(val) for key, val in calib.items()]))
+
+        return train, calib
+
+
+    # get a dictionary of image_ids and their corresponding target variable (label)
+    def get_labels(self, label_var='cancer'):
+
+        labels = {}
+        for image_id in self.metadata.image_id.unique():
+            labels[image_id] = self.metadata[self.metadata['image_id']==image_id][label_var]
+
+        return labels      
+
+
+    # get a list of image_ids from a set of patient_ids
+    def extract_image_id(self, patient_ids):
+
+        image_ids = []
+        for patient_id in patient_ids:
+            imgs = self.metadata[self.metadata['patient_id']==patient_id]['image_id'].to_list()
+            image_ids.extend(imgs)
+
+        return image_ids
+
+
+    # split data into cross-validation folds
+    def get_cv(self, n_splits=5, random_state=RANDOM_STATE):
+
+        patient_ids = self.trainset.keys()
+        df = self.metadata[self.metadata.patient_id.isin(patient_ids)]
+        
+        X, y = self.cancer_by_patient(df)
+        skf = StratifiedKFold(n_splits=n_splits, random_state=random_state, shuffle=True)
+        
+        if self.verbose==True:
+            print('Splitting training set into {} stratified k-folds...'.format(skf.get_n_splits(X, y)))
+
+        cv_patient = {}
+        cv_img = {}
+
+        for i, (train_indices, validate_indices) in enumerate(skf.split(X, y)):
+            
+            train_patients = X[X.index.isin(train_indices)]['patient_id'].to_list()
+            validate_patients = X[X.index.isin(validate_indices)]['patient_id'].to_list()
+
+            cv_patient[i] = {'train': train_patients, 'validate': validate_patients}
+
+            train_img = self.extract_image_id(train_patients)
+            validate_img = self.extract_image_id(validate_patients)
+            
+            cv_img[i] = {'train': train_img, 'validate': validate_img}
+
+            if self.verbose==True:
+                print('--Fold: ', i)
+                print('----Total patient_id in training set: ', len(cv_patient[i]['train']))
+                print('----Total image_id in training set: ', len(cv_img[i]['train']))
+
+                print('----Total patient_id in validation set: ', len(cv_patient[i]['validate']))
+                print('----Total image_id in validation set: ', len(cv_img[i]['validate']))
+
+        return cv_patient, cv_img
+
+
+    # split calibration set into training and validation sets
+    def get_calib_split(self, random_state=RANDOM_STATE):
+
+        calib_ids = self.calibset.keys()
+        X, y = self.cancer_by_patient(id_subset=calib_ids)
+        X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.20, random_state=random_state, stratify=y)
+
+        if self.verbose==True:
+            print('Total patient_id in calibration training set: ', X_train.patient_id.nunique())
+            print('Total patient_id in calibration validation set: ', X_valid.patient_id.nunique())
+
+        train = self.get_image_id(X_train)
+        valid = self.get_image_id(X_valid)
+
+        if self.verbose==True:
+            print('Total image_id in calibration training set: ', sum([len(val) for key, val in train.items()]))
+            print('Total image_id in calibration validation set: ', sum([len(val) for key, val in valid.items()]))
+
+        return train, valid
---
+++ feature_extraction.py
@@ -0,0 +1,317 @@
+import numpy as np
+from sklearn.decomposition import IncrementalPCA, MiniBatchNMF
+import os
+import pickle
+import time
+
+import data_loader as dl
+
+
+EXTRACTOR_PATH = '' # directory to save trained models
+
+
+# class to calculate and keep running mean and variance in batch training
+# adapted from https://notmatthancock.github.io/2017/03/23/simple-batch-stat-updates.html
+
+class StatsRecorder:
+    def __init__(self, data=None):
+        """
+        data: ndarray, shape (nobservations, ndimensions)
+        """
+        if data is not None:
+            data = np.atleast_2d(data)
+            self.mean = data.mean(axis=0)
+            self.var  = data.var(axis=0)
+            self.nobservations = data.shape[0]
+            self.ndimensions   = data.shape[1]
+        else:
+            self.nobservations = 0
+
+    def update(self, data):
+        """
+        data: ndarray, shape (nobservations, ndimensions)
+        """
+        if self.nobservations == 0:
+            self.__init__(data)
+        else:
+            data = np.atleast_2d(data)
+            if data.shape[1] != self.ndimensions:
+                raise ValueError("Data dims don't match prev observations.")
+
+            newmean = data.mean(axis=0)
+            newvar  = data.var(axis=0)
+
+            m = self.nobservations * 1.0
+            n = data.shape[0]
+
+            tmp = self.mean
+
+            self.mean = m/(m+n)*tmp + n/(m+n)*newmean
+            self.var  = m/(m+n)*self.var + n/(m+n)*newvar +\
+                        m*n/(m+n)**2 * (tmp - newmean)**2
+
+            self.nobservations += n
+
+
+
+
+
+def train_feature_extractor(
+        img_gen, model_name,
+        n_components=None,
+        init=None, beta_loss=None, alpha_W=None, l1_ratio=None,
+        save_path=None, verbose=True):
+    
+    if model_name=='PCA':
+        model = IncrementalPCA(n_components=n_components)
+    elif model_name=='NMF':
+        model = MiniBatchNMF(n_components=n_components, init=init, beta_loss=beta_loss, alpha_W=alpha_W, l1_ratio=l1_ratio)
+
+    not verbose or print('Initialized {} with n_components = {}'.format(model_name, n_components))
+
+    i = 1
+    for batch in img_gen:
+        not verbose or print('--Training batch ', i)
+        model.partial_fit(batch)
+        i+=1
+
+    if save_path:
+        with open(save_path,'wb') as f:
+            pickle.dump(model, f)
+
+    return model
+
+
+
+
+
+def evaluate_feature_extractor(img_gen, trained_model, verbose=True):
+    
+    batch_stats = StatsRecorder()
+    diff_stats = StatsRecorder()
+
+    i = 1
+    for batch in img_gen:
+
+        batch_stats.update(batch)
+
+        not verbose or print('--Transforming batch ', i)
+        X_transformed = trained_model.transform(batch)
+            
+        not verbose or print('---Reconstructing batch')
+        X_reconstructed = trained_model.inverse_transform(X_transformed)
+
+        diff_stats.update(batch - X_reconstructed)
+        
+        if i==1:
+            diff2 = np.sum((batch - X_reconstructed)**2)
+            x2 = np.sum(batch**2)
+
+        else:
+            diff2 = diff2 + np.sum((batch - X_reconstructed)**2)
+            x2 = x2 + np.sum(batch**2)
+
+        i+=1
+
+    not verbose or print('>> Calculating explained variance...')
+    explained_var = np.mean(1 - (diff_stats.var/batch_stats.var))
+    not verbose or print('--> ', explained_var)
+
+    not verbose or print('>> Calculating reconstruction error...')
+    reconstruction_err = diff2 / x2
+    not verbose or print('--> ', reconstruction_err)
+
+    return explained_var, reconstruction_err
+
+
+
+
+
+def cv_train_feature_extractor(
+        cv_img, patient_img_dict, batch_size, basepath, img_size, normalize, random_state,
+        model_name,n_components, init=None, beta_loss=None, alpha_W=None, l1_ratio=None,
+        from_numpy=True, extractor_path=EXTRACTOR_PATH, verbose=True, evaluate=False, return_none=False    
+        ):
+
+    models = {}
+    training_time = {}
+    explained_vars = {}
+    reconstruction_errs = {}
+    
+    
+    for fold in cv_img:
+        
+        not verbose or print('Fold: ', fold)
+        savepath = extractor_path +  '/' + model_name + '_' + str(n_components) + '_' + str(random_state) + '_' + str(img_size[0]) + '_' +str(fold) + '.pkl'
+
+        # check if already exist
+        if os.path.exists(savepath):
+            print('{} model trained on this cross-validation fold already exist'.format(model_name))
+            print('-->', savepath)
+            
+            model = pickle.load(open(savepath, 'rb'))
+            training_time[fold] = 0 
+        
+        else:
+
+            train_img_ids = cv_img[fold]['train']  # list of image_id
+
+            start = time.time()
+            
+            if from_numpy==True:
+                
+                train_img_gen = dl.ImgGen_from_numpy(
+                    list_IDs = train_img_ids,
+                    batch_size = batch_size,
+                    basepath = basepath,
+                    img_size = img_size,
+                    normalize = normalize
+                )
+                
+            else:
+                       
+                train_img_gen = dl.ImgGenerator(
+                list_IDs = train_img_ids,
+                patient_img_dict = patient_img_dict,
+                batch_size = batch_size,
+                basepath = basepath,
+                img_size = img_size,
+                normalize = normalize
+                )
+                
+
+            model = train_feature_extractor(
+                img_gen = train_img_gen,
+                model_name = model_name,
+                n_components = n_components,
+                init = init,
+                beta_loss = beta_loss,
+                alpha_W = alpha_W,
+                l1_ratio = l1_ratio,
+                save_path = savepath,
+                verbose = verbose
+                )
+            
+            stop = time.time()
+            training_time[fold] = start - stop  
+
+        models[fold] = model  
+        not verbose or print('-->> Training time: ', (training_time[fold]))
+
+        if evaluate:
+
+            val_img_ids = cv_img[fold]['validate']  # list of image_id
+            
+            if from_numpy==True:
+                
+                train_img_gen = dl.ImgGen_from_numpy(
+                    list_IDs = train_img_ids,
+                    batch_size = batch_size,
+                    basepath = basepath,
+                    img_size = img_size,
+                    normalize = normalize
+                )
+                
+            else:
+                
+                val_img_gen = dl.ImgGenerator(
+                    list_IDs = val_img_ids,
+                    patient_img_dict = patient_img_dict,
+                    batch_size = batch_size,
+                    basepath = basepath,
+                    img_size = img_size,
+                    normalize = normalize
+                )
+
+                explained_var, reconstruction_err = evaluate_feature_extractor(val_img_gen, model, verbose=verbose)
+
+            explained_vars[fold] = explained_var
+            reconstruction_errs[fold] = reconstruction_err
+
+    if evaluate == True:
+        return models, training_time, explained_vars, reconstruction_errs            
+            
+    if return_none: # ignored if evaluate = True
+        return training_time
+            
+    return models, training_time
+
+
+
+
+# def cv_train_feature_extractor(
+#         cv_img, patient_img_dict, batch_size, basepath, img_size, normalize, random_state, model_name, n_components, 
+#         extractor_path=EXTRACTOR_PATH, verbose=True, evaluate=False, return_none=False    
+#         ):
+
+#     models = {}
+#     training_time = {}
+#     explained_vars = {}
+#     reconstruction_errs = {}
+
+#     for fold in cv_img:
+        
+#         not verbose or print('Fold: ', fold)
+#         savepath = extractor_path +  '/' + model_name + '_' + str(n_components) + '_' + str(random_state) + '_' + str(fold) + '.pkl'
+
+#         # check if already exist
+#         if os.path.exists(savepath):
+#             print('{} model trained on this cross-validation fold already exist'.format(model_name))
+#             print('-->', savepath)
+            
+#             model = pickle.load(open(savepath, 'rb'))
+        
+#         else:
+
+#             train_img_ids = cv_img[fold]['train']  # list of image_id
+
+#             start = time.time()
+#             train_img_gen = dl.ImgGenerator(
+#                 list_IDs = train_img_ids,
+#                 patient_img_dict = patient_img_dict,
+#                 batch_size = batch_size,
+#                 basepath = basepath,
+#                 img_size = img_size,
+#                 normalize = normalize
+#                 )
+
+#             model = train_feature_extractor(
+#                 img_gen = train_img_gen,
+#                 model_name = model_name,
+#                 n_components = n_components,
+#                 save_path = savepath,
+#                 verbose = verbose
+#                 )
+#             stop = time.time()
+
+#         models[fold] = model
+#         training_time[fold] = start - stop    
+#         not verbose or print('-->> Training time: ', (start - stop))
+
+#         if evaluate:
+
+#             val_img_ids = cv_img[fold]['validate']  # list of image_id
+
+#             val_img_gen = dl.ImgGenerator(
+#                 list_IDs = val_img_ids,
+#                 patient_img_dict = patient_img_dict,
+#                 batch_size = batch_size,
+#                 basepath = basepath,
+#                 img_size = img_size,
+#                 normalize = normalize
+#             )
+
+#             explained_var, reconstruction_err = evaluate_feature_extractor(val_img_gen, model, verbose=verbose)
+
+#             explained_vars[fold] = explained_var
+#             reconstruction_errs[fold] = reconstruction_err
+
+#     if evaluate == True:
+#         return models, training_time, explained_vars, reconstruction_errs            
+            
+#     if return_none: # ignored if evaluate = True
+#         return training_time
+            
+#     return models, training_time
+
+
